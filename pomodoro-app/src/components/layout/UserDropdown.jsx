897dcee653274bc28bd461881183c8824e1b3a31
Christian Loyola Soto <christian_loso@hotmail.com>
2025-06-14 00:51:56 -0400

    Update UserDropdown.jsx

--- pomodoro-app/src/components/layout/UserDropdown.jsx
+++ pomodoro-app/src/components/layout/UserDropdown.jsx
@@ -32,7 +32,7 @@ const UserDropdown = ({ greeting }) => {
             Mi Perfil
           </button>
           <button className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            Historial de Compras
+            Historial de Pomos
           </button>
           <button
             onClick={handleLogout}
